f31a19df537282649ec8bd2ae7336263aa557e10
Lorenzo Olearo <lorenzo.olearo@olearo.com>
2025-09-12 17:25:38 +0000

    Run flow training on all smal dataset

--- scripts/run_smal.py
+++ scripts/run_smal.py
@@ -2,82 +2,109 @@ import subprocess
 import os
 import numpy as np
 import json
+import argparse
+
+from pathlib import Path
+from typing import List
+
+OUTPUT_DIR = Path('./out/flows/smal/smal-norm-0-1-indipendent')
+SMAL_DIR = Path('./data/SMAL_r/off')
 
 
-def run_shapes():
-    # Base directory for outputs
-    output_base_dir = "smal_experiments/smal_wks_only/"
-    
-    # Create output directories if they don't exist
-    os.makedirs(output_base_dir, exist_ok=True)
-    
-    # Loop through shape indices from 80 to 99
-    for file in os.listdir("../datasets/meshes/SMAL_r/off/"):
+def get_targets(overwrite) -> List[str]:
+    targets = []
+
+    for file in os.listdir(SMAL_DIR):
         if file.endswith(".off"):
             shape_name = os.path.splitext(file)[0]
-            # Create specific output directory for this shape
-            output_dir = f"{output_base_dir}/{shape_name}"
-            log_dir = f"{output_base_dir}/{shape_name}"
-            
-            # Create directories if they don't exist
-            os.makedirs(output_dir, exist_ok=True)
-            os.makedirs(log_dir, exist_ok=True)
+            os.makedirs(OUTPUT_DIR, exist_ok=True)
+            os.makedirs(Path(OUTPUT_DIR, shape_name), exist_ok=True)
 
-            
-            lm_smal=np.array([3162, 1931, 3731, 1399 ,1111, 1001])
+            if not os.path.exists(Path(OUTPUT_DIR, shape_name, 'checkpoint-9999.pth')) or overwrite:
+                targets.append(shape_name)
 
-            corr=np.array(np.loadtxt('../datasets/meshes/SMAL_r/corres/'+shape_name+'.vts'))-1
-            lm = corr[lm_smal]
+    return targets
 
 
-            # Build command with all parameters
-            # Prepare config dictionary
-            config = {
-                "blr": 5e-7,
-                "output_dir": output_dir,
-                "log_dir": log_dir,
-                "data_path": f"../datasets/meshes/SMAL_r/off/{shape_name}.off",
-                "epochs": 10000,
-                "num_step": 64,
-                "method": "FM",
-                "network": "MLP_general",
-                "batch_size": 10000,
-                "num_points_train": 10000,
-                "learning_rate": 0.01,
-                "distribution": "Gaussian",
-                "train": True,
-                "embedding_dim": 10,
-                "embedding": "features_only",
-                "features_type": "wks_plus_ldmk",
-                "device": "cuda:1",
-                "landmarks": lm.tolist(),
-            }
+def main(args):
+    targets = get_targets(overwrite=args.overwrite)
+    if targets == []:
+        print("No targets found, exiting.")
+        exit(0)
+    print(f"Processing {len(targets)} targets: {targets}")
 
-            # Write config to a temporary file
-            config_path = os.path.join(output_dir, "config.json")
-            with open(config_path, "w") as f:
-                json.dump(config, f, indent=4)
+    for target in targets:
+        target_dir = Path(OUTPUT_DIR, target)
 
-            # Build command to call main_smal.py with config
+        working_dir = Path(str(Path(__file__).resolve()).split('/scripts')[0])
+        data_path = Path(working_dir, SMAL_DIR, f"{target}.off")
+        features_path = Path(working_dir, 'data', 'SMAL_features_pca_20', f"{target}_features.npy")
+
+        smal_landmarks = np.array([3162, 1931, 3731, 1399 ,1111, 1001])
+        corr = np.array(np.loadtxt(f'./data/SMAL_r/corres/{target}.vts')) - 1
+        target_landmarks = corr[smal_landmarks]
+        
+        config = {
+            "device": "cuda:1",
+            "blr": 5e-7,
+            "output_dir": str(target_dir),
+            "log_dir": str(target_dir),
+            "data_path": str(data_path),
+            "train": True,
+            "inference": True,
+            "epochs": 10000,
+            "num_steps": 64,
+            "method": "FM",
+            "network": "MLP",
+            "batch_size": 50000,
+            "num_points_train": 50000,
+            "learning_rate": 0.01,
+            "distribution": "gaussian",
+            "embedding_dim": 6,
+            "embedding_type": "features_only",
+            "features_type": "landmarks",
+            "landmarks": target_landmarks.tolist(),
+        }
+
+        config_path = os.path.join(target_dir, "config.json")
+        with open(config_path, 'w') as f:
+            json.dump(config, f, indent=4)
+
+        if args.external is True:
             command = [
-                "python", "/home/ubuntu/FlowMatching4Matching/main.py",
-                "--config", config_path
+                "python", "main.py",
+                "--config", config_path,
+                "--features_path", str(features_path),
+                "--features_interpolation", str(500000),
             ]
-            # Check if the checkpoint file exists
-            checkpoint_path = os.path.join(output_dir, 'checkpoint-9999.pth')
-            if os.path.exists(checkpoint_path):
-                print(f"Skipping {shape_name} as checkpoint file exist.")
-            else:
-                # Convert command list to string for printing
-                command_str = " ".join(command)
-                print(f"Running command: {command_str}")
-                
-                # Execute the command
-                try:
-                    subprocess.run(command, check=True)
-                    print(f"Successfully processed {shape_name}")
-                except subprocess.CalledProcessError as e:
-                    print(f"Error processing {shape_name}: {e}")
+        else:
+            command = [
+                "python", "main.py",
+                "--config", config_path,
+                "--features_normalization", "0_1_indipendent"
+            ]
+
+        command_str = " ".join(command)
+        print(f"Running command: {command_str}")
+
+        try:
+            subprocess.run(command, check=True)
+            print(f"Successfully processed {target}")
+        except subprocess.CalledProcessError as e:
+            print(f"Error processing {target}: {e}")
+
+
 
 if __name__ == "__main__":
-    run_shapes()
+    parser = argparse.ArgumentParser(description="Train a flow on all SMAL meshes")
+    parser.add_argument('--overwrite', action='store_true', help="Overwrite if an existing flow model \"checkpoint-9999.pth\" is found", default='False')
+    parser.add_argument('--external', action='store_true', help="Use external precomputed features", default='False')
+
+    args = parser.parse_args()
+
+    print("Training flows on FAUST dataset:")
+    for arg, value in vars(args).items():
+        print(f"  {arg}: {value}")
+    print("-----------------------------------------------")
+
+    main(args)
