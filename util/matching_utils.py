4a4741d70c05838ca4229e7a75fad310411f22b3
gviga <giulio.vigan@gmail.com>
2025-09-19 09:11:50 +0000

    implementation of baselines

--- util/matching_utils.py
+++ util/matching_utils.py
@@ -32,7 +32,12 @@ device = "cuda:0"
 torch.cuda.set_device(device)
 
 
-def compute_p2p_with_flows_composition(source_input, target_input, source_model, target_model, device):
+##############################   OURS AND VARIATIONS   #######################################
+
+
+def compute_p2p_with_flows_composition(
+    source_input, target_input, source_model, target_model, device
+):
     """
     Compute point-to-point maps using flow composition.
 
@@ -63,7 +68,9 @@ def compute_p2p_with_flows_composition(source_input, target_input, source_model,
     return p2p
 
 
-def compute_p2p_with_flows_composition_hungarian(source_input, target_input, source_model, target_model):
+def compute_p2p_with_flows_composition_hungarian(
+    source_input, target_input, source_model, target_model
+):
     """
     Compute point-to-point maps using flow composition with Hungarian algorithm.
 
@@ -91,7 +98,9 @@ def compute_p2p_with_flows_composition_hungarian(source_input, target_input, sou
     return p2p
 
 
-def compute_p2p_with_flows_composition_lapjv(source_input, target_input, source_model, target_model):
+def compute_p2p_with_flows_composition_lapjv(
+    source_input, target_input, source_model, target_model
+):
     """
     Compute point-to-point maps using flow composition with Hungarian algorithm.
 
@@ -114,8 +123,9 @@ def compute_p2p_with_flows_composition_lapjv(source_input, target_input, source_
     return p2p
 
 
-def compute_p2p_with_flows_composition_hungarian_optimized(source_input, target_input, source_model, target_model):
-    
+def compute_p2p_with_flows_composition_hungarian_optimized(
+    source_input, target_input, source_model, target_model
+):
     with torch.no_grad():
         emb1_pullback = source_model.inverse(samples=source_input, num_steps=64)
         sample = target_model.sample(noise=emb1_pullback, num_steps=64)
@@ -141,7 +151,9 @@ def compute_p2p_with_flows_composition_hungarian_optimized(source_input, target_
     return p2p
 
 
-def compute_p2p_with_inverted_flows_in_gauss(source_input, target_input, source_model, target_model):
+def compute_p2p_with_inverted_flows_in_gauss(
+    source_input, target_input, source_model, target_model
+):
     """
     Compute point-to-point maps using nearest neighbor between the gaussians.
     Args:
@@ -163,7 +175,9 @@ def compute_p2p_with_inverted_flows_in_gauss(source_input, target_input, source_
     return p2p
 
 
-def compute_p2p_with_inverted_flows_in_gauss_uniformed(source_input, target_input, source_model, target_model):
+def compute_p2p_with_inverted_flows_in_gauss_uniformed(
+    source_input, target_input, source_model, target_model
+):
     """
     Compute point-to-point maps using nearest neighbor between the gaussians.
     Args:
@@ -189,6 +203,9 @@ def compute_p2p_with_inverted_flows_in_gauss_uniformed(source_input, target_inpu
     return p2p
 
 
+################ KNN #######################
+
+
 def compute_p2p_with_knn(source_input, target_input):
     """
     Compute point-to-point maps using nearest neighbor between features.
@@ -205,6 +222,7 @@ def compute_p2p_with_knn(source_input, target_input):
     return p2p
 
 
+################ LINEAR ASSIGNMENT #######################
 def compute_p2p_with_hungarian(source_input, target_input):
     """
     Compute point-to-point maps using Hungarian algorithm between features.
@@ -247,9 +265,12 @@ def compute_p2p_with_lapjv(source_input, target_input):
     return p2p
 
 
+################ FUNCTIONAL MAPS #######################
+
+
 def compute_p2p_with_fmaps(source_path, target_path, source_features, target_features):
     """
-    Compute point-to-point maps using functional maps.
+    Compute point-to-point maps using functional maps optimized on initial features.
     Args:
         source_path: Path to the source mesh
         target_path: Path to the target mesh
@@ -269,8 +290,8 @@ def compute_p2p_with_fmaps(source_path, target_path, source_features, target_fea
         mesh_a = PointCloud(np.array(mesh.vertices))
         mesh_b = PointCloud(np.array(mesh2.vertices))
 
-    mesh_a.laplacian.laplaceian_finder = LaplacianFinder.from_registry(which="robust")
-    mesh_b.laplacian.laplaceian_finder = LaplacianFinder.from_registry(which="robust")
+    mesh_a.laplacian.laplacian_finder = LaplacianFinder.from_registry(which="robust")
+    mesh_b.laplacian.laplacian_finder = LaplacianFinder.from_registry(which="robust")
 
     mesh_a.laplacian.find_spectrum(spectrum_size=20)
     mesh_b.laplacian.find_spectrum(spectrum_size=20)
@@ -318,6 +339,374 @@ def compute_p2p_with_fmaps(source_path, target_path, source_features, target_fea
     p2p = converter(fmap, mesh_b.basis, mesh_a.basis)
 
     return p2p
+
+
+from geomfum.descriptor.spectral import (
+    WaveKernelSignature,
+    LandmarkWaveKernelSignature,
+)
+from geomfum.descriptor.pipeline import (
+    DescriptorPipeline,
+    ArangeSubsampler,
+    L2InnerNormalizer,
+)
+
+
+def compute_p2p_with_fmaps_wks(source_path, target_path, source_ldm, target_landmark):
+    """
+    Compute point-to-point maps using functional maps.
+    Args:
+        source_path: Path to the source mesh
+        target_path: Path to the target mesh
+        input_landmark: Input landmark indices (n_ldmk)
+        target_landmark: Target landmark indices (n_ldmk)
+    """
+    mesh = trimesh.load(source_path, process=False)
+    mesh2 = trimesh.load(target_path, process=False)
+
+    if len(mesh.faces) > 0:
+        mesh = normalize_mesh(trimesh.load(source_path, process=False))
+        mesh2 = normalize_mesh(trimesh.load(target_path, process=False))
+
+        mesh_a = TriangleMesh(np.array(mesh.vertices), np.array(mesh.faces))
+        mesh_b = TriangleMesh(np.array(mesh2.vertices), np.array(mesh2.faces))
+    else:
+        mesh_a = PointCloud(np.array(mesh.vertices))
+        mesh_b = PointCloud(np.array(mesh2.vertices))
+
+    mesh_a.laplacian.laplacian_finder = LaplacianFinder.from_registry(which="robust")
+    mesh_b.laplacian.laplacian_finder = LaplacianFinder.from_registry(which="robust")
+
+    mesh_a.laplacian.find_spectrum(spectrum_size=200)
+    mesh_b.laplacian.find_spectrum(spectrum_size=200)
+    mesh_b.basis.use_k = 200
+    mesh_a.basis.use_k = 200
+
+    mesh_a.landmark_indices = source_ldm
+    mesh_b.landmark_indices = target_landmark
+
+    steps = [
+        WaveKernelSignature(n_domain=200, k=200),
+        LandmarkWaveKernelSignature(n_domain=200, k=200),
+        ArangeSubsampler(subsample_step=10),
+        L2InnerNormalizer(),
+    ]
+
+    pipeline = DescriptorPipeline(steps)
+
+    descr_a = pipeline.apply(mesh_a)
+    descr_b = pipeline.apply(mesh_b)
+
+    mesh_b.basis.use_k = 20
+    mesh_a.basis.use_k = 20
+
+    factors = [
+        SpectralDescriptorPreservation(
+            mesh_b.basis.project(descr_b),
+            mesh_a.basis.project(descr_a),
+            weight=1.0,
+        ),
+        LBCommutativityEnforcing.from_bases(
+            mesh_b.basis,
+            mesh_a.basis,
+            weight=1e-2,
+        ),
+        OperatorCommutativityEnforcing.from_multiplication(
+            mesh_b.basis, descr_b, mesh_a.basis, descr_a, weight=1e-1
+        ),
+    ]
+
+    objective = FactorSum(factors)
+
+    x0 = np.zeros((mesh_a.basis.spectrum_size, mesh_b.basis.spectrum_size))
+    optimizer = ScipyMinimize(
+        method="L-BFGS-B",
+    )
+
+    res = optimizer.minimize(
+        objective,
+        x0,
+        fun_jac=objective.gradient,
+    )
+
+    fmap = res.x.reshape(x0.shape)
+
+    fmap.shape
+
+    converter = P2pFromFmConverter()
+
+    p2p = converter(fmap, mesh_a.basis, mesh_b.basis)
+
+    return p2p
+
+
+from geomfum.refine import ZoomOut, NeuralZoomOut
+from geomfum.convert import FmFromP2pConverter
+
+
+def compute_p2p_with_knn_zoomout(source_path, target_path, source_input, target_input):
+    """
+    knn + zoomout
+    Compute point-to-point maps using functional maps optimized on initial features.
+    Args:
+        source_path: Path to the source mesh
+        target_path: Path to the target mesh
+        source_input: Source features (N, D)
+        target_input: Target features (M, D)
+    """
+    nbrs = NearestNeighbors(n_neighbors=1, algorithm="kd_tree").fit(
+        target_input.cpu().numpy()
+    )
+    _, p2p = nbrs.kneighbors(source_input.cpu().numpy())
+    p2p = p2p[:, 0]
+
+    mesh = trimesh.load(source_path, process=False)
+    mesh2 = trimesh.load(target_path, process=False)
+
+    if len(mesh.faces) > 0:
+        mesh = normalize_mesh(trimesh.load(source_path, process=False))
+        mesh2 = normalize_mesh(trimesh.load(target_path, process=False))
+
+        mesh_a = TriangleMesh(np.array(mesh.vertices), np.array(mesh.faces))
+        mesh_b = TriangleMesh(np.array(mesh2.vertices), np.array(mesh2.faces))
+    else:
+        mesh_a = PointCloud(np.array(mesh.vertices))
+        mesh_b = PointCloud(np.array(mesh2.vertices))
+
+    mesh_a.laplacian.laplacian_finder = LaplacianFinder.from_registry(which="robust")
+    mesh_b.laplacian.laplacian_finder = LaplacianFinder.from_registry(which="robust")
+
+    mesh_a.laplacian.find_spectrum(spectrum_size=200)
+    mesh_b.laplacian.find_spectrum(spectrum_size=200)
+    mesh_b.basis.use_k = 20
+    mesh_a.basis.use_k = 20
+
+    p2p_to_fm = FmFromP2pConverter()
+    fmap_ini = p2p_to_fm(p2p, mesh_b.basis, mesh_a.basis)
+
+    zoomout = ZoomOut(nit=20, step=5)
+    fmap = zoomout(fmap_ini, mesh_b.basis, mesh_a.basis)
+    converter = P2pFromFmConverter()
+    p2p = converter(fmap, mesh_b.basis, mesh_a.basis)
+
+    return p2p
+
+
+def compute_p2p_with_fmap_zoomout(source_path, target_path, source_input, target_input):
+    """
+    fmap + zoomout
+    Compute point-to-point maps using functional maps optimized on initial features.
+    Args:
+        source_path: Path to the source mesh
+        target_path: Path to the target mesh
+        source_input: Source features (N, D)
+        target_input: Target features (M, D)
+    """
+    mesh = trimesh.load(source_path, process=False)
+    mesh2 = trimesh.load(target_path, process=False)
+
+    if len(mesh.faces) > 0:
+        mesh = normalize_mesh(trimesh.load(source_path, process=False))
+        mesh2 = normalize_mesh(trimesh.load(target_path, process=False))
+
+        mesh_a = TriangleMesh(np.array(mesh.vertices), np.array(mesh.faces))
+        mesh_b = TriangleMesh(np.array(mesh2.vertices), np.array(mesh2.faces))
+    else:
+        mesh_a = PointCloud(np.array(mesh.vertices))
+        mesh_b = PointCloud(np.array(mesh2.vertices))
+
+    mesh_a.laplacian.laplacian_finder = LaplacianFinder.from_registry(which="robust")
+    mesh_b.laplacian.laplacian_finder = LaplacianFinder.from_registry(which="robust")
+
+    mesh_a.laplacian.find_spectrum(spectrum_size=20)
+    mesh_b.laplacian.find_spectrum(spectrum_size=20)
+    mesh_b.basis.use_k = 20
+    mesh_a.basis.use_k = 20
+
+    descr_a = source_input.cpu().numpy().astype(np.float32).T
+    descr_b = target_input.cpu().numpy().astype(np.float32).T
+
+    factors = [
+        SpectralDescriptorPreservation(
+            mesh_b.basis.project(descr_b),
+            mesh_a.basis.project(descr_a),
+            weight=1.0,
+        ),
+        LBCommutativityEnforcing.from_bases(
+            mesh_b.basis,
+            mesh_a.basis,
+            weight=1e-2,
+        ),
+        OperatorCommutativityEnforcing.from_multiplication(
+            mesh_b.basis, descr_b, mesh_a.basis, descr_a, weight=1e-1
+        ),
+    ]
+
+    objective = FactorSum(factors)
+
+    x0 = np.zeros((mesh_a.basis.spectrum_size, mesh_b.basis.spectrum_size))
+    optimizer = ScipyMinimize(
+        method="L-BFGS-B",
+    )
+
+    res = optimizer.minimize(
+        objective,
+        x0,
+        fun_jac=objective.gradient,
+    )
+
+    fmap = res.x.reshape(x0.shape)
+
+    fmap.shape
+
+    zoomout = ZoomOut(nit=20, step=5)
+    fmap = zoomout(fmap, mesh_b.basis, mesh_a.basis)
+    converter = P2pFromFmConverter()
+    p2p = converter(fmap, mesh_b.basis, mesh_a.basis)
+
+    return p2p
+
+
+def compute_p2p_with_fmap_neural_zoomout(
+    source_path, target_path, source_input, target_input
+):
+    """
+    knn + neural_zoomout
+    Compute point-to-point maps using functional maps optimized on initial features.
+    Args:
+        source_path: Path to the source mesh
+        target_path: Path to the target mesh
+        source_input: Source features (N, D)
+        target_input: Target features (M, D)
+    """
+    mesh = trimesh.load(source_path, process=False)
+    mesh2 = trimesh.load(target_path, process=False)
+
+    if len(mesh.faces) > 0:
+        mesh = normalize_mesh(trimesh.load(source_path, process=False))
+        mesh2 = normalize_mesh(trimesh.load(target_path, process=False))
+
+        mesh_a = TriangleMesh(np.array(mesh.vertices), np.array(mesh.faces))
+        mesh_b = TriangleMesh(np.array(mesh2.vertices), np.array(mesh2.faces))
+    else:
+        mesh_a = PointCloud(np.array(mesh.vertices))
+        mesh_b = PointCloud(np.array(mesh2.vertices))
+
+    mesh_a.laplacian.laplacian_finder = LaplacianFinder.from_registry(which="robust")
+    mesh_b.laplacian.laplacian_finder = LaplacianFinder.from_registry(which="robust")
+
+    mesh_a.laplacian.find_spectrum(spectrum_size=20)
+    mesh_b.laplacian.find_spectrum(spectrum_size=20)
+    mesh_b.basis.use_k = 20
+    mesh_a.basis.use_k = 20
+
+    descr_a = source_input.cpu().numpy().astype(np.float32).T
+    descr_b = target_input.cpu().numpy().astype(np.float32).T
+
+    factors = [
+        SpectralDescriptorPreservation(
+            mesh_b.basis.project(descr_b),
+            mesh_a.basis.project(descr_a),
+            weight=1.0,
+        ),
+        LBCommutativityEnforcing.from_bases(
+            mesh_b.basis,
+            mesh_a.basis,
+            weight=1e-2,
+        ),
+        OperatorCommutativityEnforcing.from_multiplication(
+            mesh_b.basis, descr_b, mesh_a.basis, descr_a, weight=1e-1
+        ),
+    ]
+
+    objective = FactorSum(factors)
+
+    x0 = np.zeros((mesh_a.basis.spectrum_size, mesh_b.basis.spectrum_size))
+    optimizer = ScipyMinimize(
+        method="L-BFGS-B",
+    )
+
+    res = optimizer.minimize(
+        objective,
+        x0,
+        fun_jac=objective.gradient,
+    )
+
+    fmap = res.x.reshape(x0.shape)
+
+    fmap.shape
+
+    zoomout = NeuralZoomOut(nit=20, step=5, device="cuda:0")
+    fmap = zoomout(fmap, mesh_b.basis, mesh_a.basis)
+    converter = P2pFromFmConverter()
+    p2p = converter(fmap, mesh_b.basis, mesh_a.basis)
+
+    return p2p
+
+
+################ Neural Deformation Pyramid #######################
+import sys
+
+sys.path.append("/home/giulio_vigano/SM-baseline/DeformationPyramid/")
+import torch
+from model.registration import Registration
+import yaml
+from easydict import EasyDict as edict
+from model.tiktok import Timers
+
+
+def ndp_with_ldmks(source_shape, target_shape, source_landmarks, target_landmarks):
+    """
+    Compute point-to-point maps using neural deformation pyramids.
+    Args:
+        source_shape: Source shape
+        target_shape: Target shape
+        source_features: Source landmarks (source_n_landmarks)
+        target_features: Target landmarks (target_n_landmarks)
+    Returns:
+        p2p: Point-to-point maps (source_n_points)
+    """
+
+    source_shape.landmark_indices = source_landmarks
+    target_shape.landmark_indices = target_landmarks
+
+    with open(
+        "DeformationPyramid/config/NDP.yaml",
+        "r",
+    ) as f:
+        config = yaml.load(f, Loader=yaml.Loader)
+
+    config = edict(config)
+
+    # backup the experiment
+    if config.gpu_mode:
+        config.device = torch.cuda.current_device()
+    else:
+        config.device = torch.device("cpu")
+
+    model = Registration(config)
+    timer = Timers()
+    x, y = (
+        torch.tensor(source_shape.vertices).float().to("cuda"),
+        torch.tensor(target_shape.vertices).float().to("cuda"),
+    )
+
+    model.load_pcds(
+        x, y, [x[source_shape.landmark_indices], y[target_shape.landmark_indices]]
+    )
+
+    timer.tic("registration")
+    warped_pcd, iter_cnt, timer = model.register(timer=timer)
+    timer.toc("registration")
+
+    nbrs = NearestNeighbors(n_neighbors=1, algorithm="auto").fit(y.cpu().numpy())
+    _, p2p = nbrs.kneighbors(warped_pcd.cpu().numpy())
+    p2p = p2p[:, 0]
+
+    return p2p
+
+
+################ OT #######################
 
 
 def compute_p2p_with_ot(source_features, target_features):
